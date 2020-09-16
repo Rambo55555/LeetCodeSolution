fb420015ab31190241686449bcbdaef3d4e61e29
Rambo <1434694372@qq.com>
2020-09-16 21:42:54 +0800

    1

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Rambo
- * @date 2020-09-16 20:17
- */
-public class Main {
-    public static void main(String[] args){
-        System.out.println("hello");
-    }
-}
---
+++ src/Solution.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Rambo
+ * @date 2020-09-16 20:17
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int result[] = new int[1];
+
+        return result;
+    }
+    public static void main(String[] args){
+        int a[] = new int[2047];
+        System.out.println(2047 & 1);
+        System.out.println(2047 & 2048);
+    }
+}
