08f0b03775ead2f3d32a7798af8cb6784ddcae94
Rambo <1434694372@qq.com>
2021-01-14 16:19:04 +0800

    316

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,35 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 /**
  * @author Rambo
  * @date 2020-09-16 20:17
  */
 public class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int result[] = new int[1];
+    public static String removeDuplicateLetters(String s) {
+        String result = "";
+        Stack<Character> stack = new Stack<>();
 
-        return result;
+        for(int i = 0; i < s.length(); ++i){
+            if(stack.contains(s.charAt(i))){
+                continue;
+            }
+            while(!stack.isEmpty() && s.charAt(i) < stack.peek() && s.indexOf(stack.peek(), i) > 0){
+                stack.pop();
+            }
+            stack.push(s.charAt(i));
+        }
+        char[] res = new char[stack.size()];
+        for(int i = 0; i < stack.size(); i++)
+            res[i] = stack.get(i);
+
+        return new String(res);
     }
+
+
     public static void main(String[] args){
-        int a[] = new int[2047];
-        System.out.println(2047 & 1);
-        System.out.println(2047 & 2048);
+        System.out.println(removeDuplicateLetters("bcbac"));
     }
 }
