bf07820150548b3373ee2ab1d44525eaca5bb057
Rambo <1434694372@qq.com>
2021-02-08 10:28:35 +0800

    57

--- src/Solution.java
+++ src/Solution.java
@@ -8,36 +8,16 @@ import java.util.Map;
  * @date 2020-09-16 20:17
  */
 public class Solution {
-    public static ArrayList<Integer> FindNumbersWithSum(int [] array, int sum) {
-        int p = 0, q = array.length - 1;
-        ArrayList<Integer> resultList = new ArrayList();
-        //int[] resultList = new int[2];
-        int mul = Integer.MAX_VALUE;
-        while(p < q) {
-            if (array[p] + array[q] == sum) {
-                if (p * q < mul){
-                    mul = p * q;
-                    resultList.clear();
-                    resultList.add(array[p++]);
-                    resultList.add(array[q--]);
-//                     resultList[0] = array[p++];
-//                     resultList[1] = array[q--];
-                }
-            } else if (array[p] + array[q] < sum) {
-                p++;
-            } else{
-                q--;
-            }
-        }
-        return resultList;
-    }
-    public static void main(String[] args){
-        int [] array = new int[]{1,2,4,7,11,17};
-        ArrayList<Integer> result = FindNumbersWithSum(array,13);
-        for (int i:result
-             ) {
-            System.out.println(i);
-        }
 
+
+    public static void main(String[] args){
+        String s1 = "466";
+        String s2 = "123";
+        System.out.println(s2.compareTo(s1));
+        String[] s = new String[]{"5","4","3","2"};
+        Arrays.sort(s, (a,b) -> a.compareTo(b));
+        for(String str: s){
+            System.out.println(str);
+        }
     }
 }
