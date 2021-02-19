faa9bc151b828ebb3519d693df5e7cb8f70c38b5
Rambo <1434694372@qq.com>
2021-02-19 21:31:12 +0800

    29

---
+++ src/Rambo/常用算法/Heap.java
@@ -0,0 +1,8 @@
+package Rambo.常用算法;
+
+/**
+ * @author Rambo
+ * @date 2021-02-19 20:36
+ */
+public class Heap {
+}
--- src/Rambo/常用算法/Sort.java
+++ src/Rambo/常用算法/Sort.java
@@ -1,22 +1,10 @@
-import java.util.*;
+package Rambo.常用算法;
 
 /**
  * @author Rambo
- * @date 2020-09-16 20:17
+ * @date 2021-02-19 20:24
  */
-public class Solution {
-    public static void findKth(int nums[], int k) {
-        int i = 0, j = nums.length - 1;
-        while(i < j) {
-            int p = partion(nums, i, j);
-            if(p == k) break;
-            else if(p < k) {
-                i = p + 1;
-            }else {
-                j = p - 1;
-            }
-        }
-    }
+public class Sort {
     public static int partion(int nums[], int l, int h) {
         int p = nums[l];
         int i = l, j = h+1;
@@ -41,13 +29,5 @@ public class Solution {
         int p = partion(nums, l, h);
         quickSort(nums, l, p-1);
         quickSort(nums, p+1, h);
-    }
-
-    public static void main(String[] args){
-        int [] input = {4,3,2,1,8,10,23,7,3,1,8,34};
-        quickSort(input, 0, input.length-1);
-        for(int i = 0; i< input.length; ++i) {
-            System.out.println(input[i]);
-        }
     }
 }
