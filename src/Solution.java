3ac1d2693ed3b1f89f010e62f5a8a9e80c8d2ff5
Rambo <1434694372@qq.com>
2021-02-22 16:23:06 +0800

    14 15 25 36 55

--- src/Solution.java
+++ src/Solution.java
@@ -5,49 +5,30 @@ import java.util.*;
  * @date 2020-09-16 20:17
  */
 public class Solution {
-    public static void findKth(int nums[], int k) {
-        int i = 0, j = nums.length - 1;
-        while(i < j) {
-            int p = partion(nums, i, j);
-            if(p == k) break;
-            else if(p < k) {
-                i = p + 1;
-            }else {
-                j = p - 1;
+    public static int StrToInt(String str) {
+        if(str.length() < 1) return 0;
+
+        long sum = 0;
+        boolean isPositive = str.charAt(0) != '-';
+        for(int i = 0; i < str.length(); ++i){
+            char ch = str.charAt(i);
+            if(i == 0 && (ch == '+' || ch == '-'))continue;
+            int cur = ch - 48;
+            if(cur >= 0 && cur <= 9){
+                sum = sum * 10 + cur;
+            } else{
+                return 0;
+            }
+            if((isPositive && sum > Integer.MAX_VALUE)) return 0;
+            if((!isPositive && sum > -1 * (long)Integer.MIN_VALUE)) {
+                System.out.println("HHH:"+ sum + " " + (-1 * (long)Integer.MIN_VALUE));
+                return 0;
             }
         }
-    }
-    public static int partion(int nums[], int l, int h) {
-        int p = nums[l];
-        int i = l, j = h+1;
-        while(true) {
-            while(i != h && nums[++i] < p);
-            while(j != l && nums[--j] > p);
-            if(i >= j) break;
-            swap(nums, i, j);
-        }
-        swap(nums, l, j);
-        return j;
-    }
-
-    public static void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    public static void quickSort(int[] nums, int l, int h) {
-        if(l >= h) return;
-        int p = partion(nums, l, h);
-        quickSort(nums, l, p-1);
-        quickSort(nums, p+1, h);
+        return isPositive? (int)sum : -1 * (int)sum;
     }
 
     public static void main(String[] args){
-        int [] input = {4,3,2,1,8,10,23,7,3,1,8,34};
-        quickSort(input, 0, input.length-1);
-        for(int i = 0; i< input.length; ++i) {
-            System.out.println(input[i]);
-        }
+        System.out.println(StrToInt("-123")+ " " +(Integer.MIN_VALUE * -1));
     }
 }
